fc4f2505c480f0d0c638730e896d40ce71ce032a
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-07-04 13:03:37 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import LobbyScreen from './screens/Lobby';
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
+import Roompage from './screens/Room';
 
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route path='/' element = {<LobbyScreen/>} />
+      <Route path='/room/:roomId' element = {<Roompage/>} />
 
       </Routes>
          
