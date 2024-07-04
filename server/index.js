416f87dc073aea827232cc77da417ae5a4f08bef
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-07-04 13:04:13 +0530

    update

--- server/index.js
+++ server/index.js
@@ -16,7 +16,13 @@ io.on("connection",(socket)=>{
         const {email,room}=data
         emailToSocketIdMap.set (email,socket.id)
         SocketidToEmailMap.set(socket.id,email)
+        io.to (room).emit("user:joined",{email,id: socket.id})
+        socket.join(room)
         io.to(socket.id).emit("room:join",data)
 
     })
+    socket.on ('user:call',({to,offer})=>{
+        io.to(to).emit('incomming call',{from:socket.id,offer});
+    });
+    socket.on ('call:accepted')
 });
